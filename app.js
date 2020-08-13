e06c2ab8c86dba89655a0888970759ab731f032e
pjaeyoung <saiei2484@gmail.com>
2020-08-13 16:27:42 +0900

    :shirt: style: question route path 변경 question route path를 분명히 하기 위해 room을 추가해서 변경함 /:roomId/questions => /room/:roomId/questions

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const logger = require('morgan');
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-const questionRouter = require("./routes/question");
-const roomRouter = require("./routes/room");
+const questionRouter = require('./routes/question');
+const roomRouter = require('./routes/room');
+
+app.use(
+  session({
+    secret: '@switzerland',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    method: ['GET', 'POST', 'DELETE', 'PATCH'],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.use("/:roomId", questionRouter);
-app.use("/room", roomRouter);
+app.use('/room/:roomId/questions', questionRouter);
+app.use('/room', roomRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
