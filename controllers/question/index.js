9f9451f1c4a912b01ccadd4b7b580c798f909f5d
pjaeyoung <saiei2484@gmail.com>
2020-08-14 10:43:35 +0900

    :sparkles: feat : response에서 추가적으로 room id와 title을 보내기 채팅화면에서 api 요청 시 title 필요함에 따라 추가 roomId는 이후 chat 보관 시에 사용을 위함

--- controllers/question/index.js
+++ controllers/question/index.js
@@ -11,7 +11,7 @@ module.exports = {
         include: {
           model: Room,
           where: { id: roomId, userId: 1 },
-          attributes: [],
+          attributes: ['id', 'title'],
         },
       });
       res.json({ questions });
