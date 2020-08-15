07d010e30c57189293e5078245af49394732d5d1
shinuhyun <shinuhyun@gmail.com>
2020-08-15 21:08:44 +0900

    :bug: fix : roomPatch 수정 roomPatch에서  userId와 roomId를 같이 검사해야하게끔 수정

--- controllers/room/roomPatch.js
+++ controllers/room/roomPatch.js
@@ -7,32 +7,28 @@ module.exports = {
     const { roomId, title, description, questions } = req.body;
     try {
       const room = await Room.findOne({
-        where: { userId: 1 },
+        where: { userId: 2, id: roomId },
       });
       //patch 하려는 room이 있으면 실행
       if (room) {
-        const updatedRoom = await Room.update(
+        await Room.update(
           { title: title, description: description },
           { where: { id: roomId } }
         );
         await Question.destroy({
           where: { roomId: roomId },
         });
-
         questions.forEach((question) =>
           room.setQuestions(Question.create({ text: question, roomId: roomId }))
         );
-
-        if (updatedRoom) {
-          res.status(200).send("Room is edited");
-        } else {
-          res.status(404).send({ message: "Room Not Found" });
-        }
+        res.status(200).send("Room is edited");
       } else {
-        res.status(401).send({ message: "Unauthorized User" });
+        res.status(404).send({ message: "Room Not Found" });
       }
     } catch (err) {
       res.status(500).send({ message: "Server Error" });
     }
   },
 };
+//session-id 검증 결과로 나오게 됩니다.
+// res.status(401).send({ message: "Unauthorized User" });
