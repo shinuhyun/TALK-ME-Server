b477a8dd70e1401448a2197f7f7094532618db7e
shinuhyun <shinuhyun@gmail.com>
2020-08-16 17:02:31 +0900

    :sparkles: feat : session-id 검증 과정 추가 roomCreate, roomDelete, roomGet, roomPatch, roomList, question API에서 session-id 검증 과정을 추가했습니다.

--- controllers/room/roomDelete.js
+++ controllers/room/roomDelete.js
@@ -1,32 +1,37 @@
-const { Room, User } = require('../../models');
+const { Room, User } = require("../../models");
 module.exports = {
   delete: async (req, res) => {
+    const sess = req.session;
     const { roomId } = req.body;
     try {
-      // roomId를 유저가 소유하고 있는지 확인
-      const isOwn = !!(await Room.findOne({
-        attributes: ['id'],
-        where: { id: roomId },
-        include: {
-          model: User,
-          where: { id: 1 },
-        },
-      }));
-      // roomId를 소유한 유저면 destroy 실행
-      if (isOwn) {
-        await Room.destroy({
+      if (sess.userId) {
+        // roomId를 유저가 소유하고 있는지 확인
+        const isOwn = !!(await Room.findOne({
+          attributes: ["id"],
           where: { id: roomId },
-        });
-        // 삭제가 성공되었으면 200 응답
-        res.status(200).json({ message: 'Room deleted' });
+          include: {
+            model: User,
+            where: { id: sess.userId },
+          },
+        }));
+        // roomId를 소유한 유저면 destroy 실행
+        if (isOwn) {
+          await Room.destroy({
+            where: { id: roomId },
+          });
+          // 삭제가 성공되었으면 200 응답
+          res.status(200).json({ message: "Room deleted" });
+        } else {
+          // roomId를 소유하지 않았거나 삭제가 실패했을 경우
+          res.status(404).json({ message: "Room Not Found" });
+        }
       } else {
-        // roomId를 소유하지 않았거나 삭제가 실패했을 경우
-        res.status(404).json({ message: 'Room Not Found' });
+        res.status(401).send({ message: "Unauthorized User" });
       }
     } catch (err) {
       // server error handling
       console.log(err);
-      res.status(500).json({ message: 'Server Error' });
+      res.status(500).json({ message: "Server Error" });
     }
   },
 };
--- controllers/room/roomPatch.js
+++ controllers/room/roomPatch.js
@@ -1,34 +1,38 @@
-const { Question, Room } = require('../../models');
+const { Question, Room } = require("../../models");
 
 module.exports = {
   patch: async (req, res) => {
-    // const sess = req.session;
+    const sess = req.session;
 
     const { roomId, title, description, questions } = req.body;
     try {
-      const room = await Room.findOne({
-        where: { userId: 1, id: roomId },
-      });
-      //patch 하려는 room이 있으면 실행
-      if (room) {
-        await Room.update(
-          { title: title, description: description },
-          { where: { id: roomId } }
-        );
-        await Question.destroy({
-          where: { roomId: roomId },
+      if (sess.userId) {
+        const room = await Room.findOne({
+          where: { userId: sess.userId, id: roomId },
         });
-        questions.forEach((question) =>
-          room.setQuestions(Question.create({ text: question, roomId: roomId }))
-        );
-        res.status(200).send('Room is edited');
+        //patch 하려는 room이 있으면 실행
+        if (room) {
+          await Room.update(
+            { title: title, description: description },
+            { where: { id: roomId } }
+          );
+          await Question.destroy({
+            where: { roomId: roomId },
+          });
+          questions.forEach((question) =>
+            room.setQuestions(
+              Question.create({ text: question, roomId: roomId })
+            )
+          );
+          res.status(200).send("Room is edited");
+        } else {
+          res.status(404).send({ message: "Room Not Found" });
+        }
       } else {
-        res.status(404).send({ message: 'Room Not Found' });
+        res.status(401).send({ message: "Unauthorized User" });
       }
     } catch (err) {
-      res.status(500).send({ message: 'Server Error' });
+      res.status(500).send({ message: "Server Error" });
     }
   },
 };
-//session-id 검증 결과로 나오게 됩니다.
-// res.status(401).send({ message: "Unauthorized User" });
