a723e0d28befa65df6ff944db5d2da4756847665
shinuhyun <shinuhyun@gmail.com>
2020-08-17 11:52:42 +0900

    :books: docs : PWD를 위한 API 기본 세팅 controllers/user에서 PWD API 파일들을 만들고 셋팅했습니다.

---
+++ controllers/user/comparekey.js
@@ -0,0 +1,9 @@
+module.exports = {
+  post: async (req, res) => {
+    try {
+      res.status(200).send({ message: "It Works!" });
+    } catch (err) {
+      res.status(500).send({ message: "Server Error" });
+    }
+  },
+};
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { userController } = require('../controllers');
+const { userController } = require("../controllers");
 
-router.post('/signup', userController.signUp.post);
-router.post('/login', userController.login.post);
-router.get('/logout', userController.logout.get);
+router.post("/signup", userController.signUp.post);
+router.post("/login", userController.login.post);
+router.get("/logout", userController.logout.get);
+
+router.post("/pwinquiry/askkey", userController.askkey.post);
+router.post("/pwinquiry/comparekey", userController.comparekey.post);
+router.patch("/pwinquiry/newpassword", userController.newpassword.patch);
 
 module.exports = router;
