e9aa41e915720965de17b39e7855dfb96e31a3a2
shinuhyun <shinuhyun@gmail.com>
2020-08-17 16:35:43 +0900

    :sparkles: feat : comparekey API 작성 controllers/pwd/comparekey.js 에서 comparekey API를 작성했습니다.

--- controllers/pwd/comparekey.js
+++ controllers/pwd/comparekey.js
@@ -1,7 +1,21 @@
+const { User } = require("../../models");
+
 module.exports = {
   post: async (req, res) => {
+    const sess = req.session;
+    const { secretKey } = req.body;
     try {
-      res.status(200).send({ message: "It Works!" });
+      const user = await User.findOne({
+        where: {
+          email: sess.email,
+          secretKey: secretKey,
+        },
+      });
+      if (user === null) {
+        res.status(404).send({ message: "Wrong Secret Key" });
+      } else {
+        res.status(200).send({ message: "Correct Secret Key" });
+      }
     } catch (err) {
       res.status(500).send({ message: "Server Error" });
     }
