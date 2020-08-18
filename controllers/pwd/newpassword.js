5492156d7279bc695c62792f7a7d4ed8de9ccbe8
shinuhyun <shinuhyun@gmail.com>
2020-08-18 13:33:35 +0900

    :bug: fix : Password 공백 처리 signup, newpassword API에서 Password가 공백일 경우 Invalide Password를 반환하게 하였습니다.

--- controllers/pwd/newpassword.js
+++ controllers/pwd/newpassword.js
@@ -5,17 +5,17 @@ module.exports = {
     const sess = req.session;
     const { password } = req.body;
     try {
-      const user = await User.findOne({
-        where: {
-          email: sess.email,
-        },
-      });
-
-      if (user === null) {
-        res.status(401).send({ message: "Unauthorized User" });
+      if (!password) {
+        res.status(400).send({ message: "Invalid Password" });
       } else {
-        await User.updatePassword(sess.email, password);
-        res.status(200).send({ message: "New Password is saved." });
+        const user = await User.findOne({ where: { email: sess.email } });
+
+        if (user === null) {
+          res.status(401).send({ message: "Unauthorized User" });
+        } else {
+          await User.updatePassword(sess.email, password);
+          res.status(200).send({ message: "New Password is saved." });
+        }
       }
     } catch (err) {
       console.log(err);
--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -4,15 +4,19 @@ module.exports = {
   post: async (req, res) => {
     const { email, password } = req.body;
     try {
-      await User.findOrCreate({
-        where: { email: email },
-        defaults: { password: password },
-      }).then(async ([user, created]) => {
-        if (!created) {
-          return res.status(409).send({ message: "email already exists" });
-        }
-        res.status(201).send({ message: "SignUp success" });
-      });
+      if (!password) {
+        res.status(400).send({ message: "Invalid Password" });
+      } else {
+        await User.findOrCreate({
+          where: { email: email },
+          defaults: { password: password },
+        }).then(async ([user, created]) => {
+          if (!created) {
+            return res.status(409).send({ message: "email already exists" });
+          }
+          res.status(201).send({ message: "SignUp success" });
+        });
+      }
     } catch (err) {
       console.log(err);
       res.status(500).send({ message: "Server Error" });
