1c8b31a0f61d9ea0ad8104323aa02bfbca3dccd5
pjaeyoung <saiei2484@gmail.com>
2020-08-19 10:16:21 +0900

    .

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
-const express = require("express");
-const session = require("express-session");
-const cors = require("cors");
-const logger = require("morgan");
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const logger = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-const questionRouter = require("./routes/question");
-const roomRouter = require("./routes/room");
-const roomListRouter = require("./routes/roomList");
-const userRouter = require("./routes/user");
-const pwdRouter = require("./routes/pwd");
+const questionRouter = require('./routes/question');
+const roomRouter = require('./routes/room');
+const roomListRouter = require('./routes/roomList');
+const userRouter = require('./routes/user');
+const pwdRouter = require('./routes/pwd');
 
 const sessionOptions = {
-  secret: "@switzerland",
+  secret: '@switzerland',
   saveUninitialized: true,
   resave: true,
   cookie: {
@@ -23,35 +23,33 @@ const sessionOptions = {
 };
 
 // production 환경일 경우
-if (app.get("env") === "production") {
-  app.set("trust proxy", 1);
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1);
   sessionOptions.cookie.secure = true; // ssl 적용
 }
 
 app.use(session(sessionOptions));
 
-if (process.env.NODE_ENV !== "test") {
-  app.use(logger("dev"));
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
 }
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
-    method: ["GET", "POST", "DELETE", "PATCH"],
+    origin: ['http://localhost:3000'],
+    method: ['GET', 'POST', 'DELETE', 'PATCH'],
     credentials: true,
   })
 );
 
 app.use(express.json());
 
-app.use("/room/:roomId/questions", questionRouter);
-app.use("/room", roomRouter);
-app.use("/roomList", roomListRouter);
-app.use("/auth", userRouter);
-app.use("/auth/pwinquiry", pwdRouter);
+app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use('/room/:roomId/questions', questionRouter);
+app.use('/room', roomRouter);
+app.use('/roomList', roomListRouter);
+app.use('/auth', userRouter);
+app.use('/auth/pwinquiry', pwdRouter);
 
 module.exports = app;
